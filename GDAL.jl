98689fc7af7795f97edcc21a2db8b3747a9ea932
Will Kearney <wkearn@gmail.com>
2014-02-10 13:29:31 -0500

    Add and modify functions needed to write raster data from Julia
    constants.jl provides an interface for certain GDAL enumerated types

--- GDAL.jl
+++ GDAL.jl
@@ -1,11 +1,28 @@
 module GDAL
 
-export Raster, open_raster
+include("constants.jl")
+
+export 
+    # Types
+    Raster,
+    # High-level API functions
+    open_raster, copy_raster, write_raster,
+    # Utilities
+    gdal_translate,
+    # Constants
+    GA_ReadOnly,GA_Update,
+    GF_Read,GF_Write,
+    GDT_Unknown,GDT_Byte,GDT_UInt16,GDT_Int16,GDT_UInt32,
+    GDT_Int32, GDT_Float32, GDT_Float64, GDT_CInt16,
+    GDT_CInt32, GDT_CFloat32, GDT_CFloat64, GDT_TypeCount
+
 
 type Raster{T}
     ptr::Ptr{Void}
     width::Int32
     height::Int32
+    transform::Array{Float64,1}
+    projection::ASCIIString
     data::Array{T,2}
 end
 
@@ -16,95 +33,172 @@ end
 
 gdal_allregister()
 
-function open_raster(input::String,band::Int)
-    dataset = gdal_open(input)
-    raster = gdal_get_raster_band(dataset,int32(band))
-    (xsize,ysize) = gdal_get_raster_size(dataset)
-    raster_type = gdal_get_raster_datatype(raster)
-    raster_jtype = raster_type_convert(raster_type)
-    data = zeros(raster_jtype,xsize,ysize)
-    gdal_raster_io!(raster, data,raster_type,xsize,ysize,false,[int32(0) int32(0)])
-    Raster(dataset,xsize,ysize,data')
-end
+## Convert from GDAL's integer type representation to Julia's
 
 function raster_type_convert(raster_type::Int32)
-    if raster_type == 0
+    if raster_type == GDT_Unknown
         raster_jtype = Any
-    elseif raster_type == 1
+    elseif raster_type == GDT_Byte
         raster_jtype = Uint8
-    elseif raster_type == 2
+    elseif raster_type == GDT_UInt16
         raster_jtype = Uint16
-    elseif raster_type == 3
+    elseif raster_type == GDT_Int16
         raster_jtype = Int16
-    elseif raster_type == 4
+    elseif raster_type == GDT_UInt32
         raster_jtype = Uint32
-    elseif raster_type == 5
+    elseif raster_type == GDT_Int32
         raster_jtype = Int32
-    elseif raster_type == 6
+    elseif raster_type == GDT_Float32
         raster_jtype = Float32
-    elseif raster_type == 7 
+    elseif raster_type == GDT_Float64
         raster_jtype == Float64
     else
-        error("Type of data is unreadable")
+        error("Type of data not yet supported")
     end
 end
 
-## GDALAllRegister
-function gdal_allregister()
-    ccall((:GDALAllRegister,"libgdal"),Void,())
+
+
+## File interaction functions
+
+# GDALOpen
+function gdal_open(input, access::Int32)
+    ccall((:GDALOpen,"libgdal"), Ptr{Void}, (Ptr{Uint8},Bool), input, access)
 end
 
-## GDALOpen
-function gdal_open(input::String)
-    ccall((:GDALOpen,"libgdal"), Ptr{Void}, (Ptr{Uint8},Bool), input, false)
+# GDALClose
+
+function gdal_close(dataset::Ptr{Void})
+    ccall((:GDALClose,"libgdal"),Void, (Ptr{Void},), dataset)
 end
 
-## GDALGetRasterBand
+## Raster Functions
+
+# GDALGetRasterBand
 function gdal_get_raster_band(dataset::Ptr{Void},band::Int32)
     ccall((:GDALGetRasterBand,"libgdal"), Ptr{Void}, (Ptr{Void},Int32), dataset,1)
 end
 
-## GDALGetRasterSize
+# GDALGetRasterSize
 function gdal_get_raster_size(dataset::Ptr{Void})
     xsize = ccall((:GDALGetRasterXSize,"libgdal"), Int32, (Ptr{Void},), dataset)
     ysize = ccall((:GDALGetRasterYSize,"libgdal"), Int32, (Ptr{Void},), dataset)
     return (xsize,ysize)
 end
 
-## GDALGetRasterDataType
+# GDALGetRasterDataType
 function gdal_get_raster_datatype(raster::Ptr{Void})
     ccall((:GDALGetRasterDataType,"libgdal"), Int32, (Ptr{Void},), raster)
 end
 
-## GDALRasterIO
-function gdal_raster_io!{T}(raster::Ptr{Void}, data::Array{T,2},raster_type::Int32,xsize::Int32,ysize::Int32, read::Bool,offset::Array{Int32,2})
-    ccall((:GDALRasterIO,"libgdal"), Ptr{Void}, (Ptr{Void}, Bool, Int32, Int32, Int32, Int32, Ptr{Void}, Int32, Int32, Int32, Int32, Int32), raster, false, offset[1], offset[2], xsize, ysize, data, xsize, ysize, raster_type, 0, 0)
+# GDALRasterIO
+# This one reads raster band into data
+function gdal_raster_io!{T}(data::Array{T,2}, raster::Ptr{Void}, raster_type::Int32,xsize::Int32,ysize::Int32,offset::Array{Int32,1})
+    ccall((:GDALRasterIO,"libgdal"), Ptr{Void}, (Ptr{Void}, Int32, Int32, Int32, Int32, Int32, Ptr{Void}, Int32, Int32, Int32, Int32, Int32), raster, GF_Read, offset[1], offset[2], xsize, ysize, data, xsize, ysize, raster_type, 0, 0)
 end
 
-## GDALGetDatasetDriver
+# This one writes data to raster band 
+function gdal_raster_io{T}(data::Array{T,2}, raster::Ptr{Void}, raster_type::Int32,xsize::Int32,ysize::Int32,offset::Array{Int32,1})
+    ccall((:GDALRasterIO,"libgdal"), Ptr{Void}, (Ptr{Void}, Int32, Int32, Int32, Int32, Int32, Ptr{Void}, Int32, Int32, Int32, Int32, Int32), raster, GF_Write, offset[1], offset[2], xsize, ysize, data, xsize, ysize, raster_type, 0, 0)
+end
+
+## Driver functions
+
+# GDALGetDatasetDriver
 
 function gdal_get_dataset_driver(dataset::Ptr{Void})
     ccall((:GDALGetDatasetDriver,"libgdal"), Ptr{Void}, (Ptr{Void},),dataset)
 end
 
-## GDALIdentifyDriver
+# GDALIdentifyDriver
 
 function gdal_identify_driver(filename::String, filelist::Array{String,1})
     ccall((:GDALIdentifyDriver,"libgdal"), Ptr{Void}, (Ptr{Uint8},Ptr{Ptr{Uint8}}), filename, filelist)
 end
+
+# GDALGetDriverLongName
+
+function gdal_get_driver_long_name(driver::Ptr{Void})
+    bytestring(ccall((:GDALGetDriverLongName,"libgdal"), Ptr{Uint8}, (Ptr{Void},),driver))
+end
+
+# GDALGetDriverByName
+
+function gdal_get_driver_by_name(driver::ASCIIString)
+    ccall((:GDALGetDriverByName,"libgdal"), Ptr{Void},(Ptr{Uint8},),driver)
+end
+
+## Transform functions
     
-## GDALGetGeoTransform
+# GDALGetGeoTransform
 function gdal_get_geotransform(dataset::Ptr{Void})
     transform = zeros(Float64,6)
     ccall((:GDALGetGeoTransform,"libgdal"), Void, (Ptr{Void},Ptr{Void}), dataset, transform)
     return transform
 end
 
-## GDALGetProjectionRef
+# GDALSetGeoTransform
 
-function gdal_get_projectionref(dataset::Ptr{Void})
-    ccall((:GDALGetProjectionRef,"libgdal"), Ptr{Uint8}, (Ptr{Void},), dataset)
+function gdal_set_geotransform(dataset::Ptr{Void},transform::Array{Float64,1})
+    ccall((:GDALSetGeoTransform,"libgdal"),Void,(Ptr{Void},Ptr{Void}),dataset,transform)
 end
 
+## Projection functions
+
+# GDALGetProjectionRef
+function gdal_get_projectionref(dataset::Ptr{Void})
+    bytestring(ccall((:GDALGetProjectionRef,"libgdal"), Ptr{Uint8}, (Ptr{Void},), dataset))
+end
+
+# GDALSetProjection
+function gdal_set_projection(dataset::Ptr{Void},projection::ASCIIString)
+    ccall((:GDALSetProjection,"libgdal"),Void,(Ptr{Void},Ptr{Uint8}),dataset,projection)
+end
+
+## File creation functions
+
+function gdal_create_copy(destination::ASCIIString,srcdataset::Ptr{Void},dstdriver::ASCIIString,bstrict::Bool,options::Array{ASCIIString,1})
+    dstdriver_ptr = gdal_get_driver_by_name(dstdriver)
+    ccall((:GDALCreateCopy,"libgdal"),Ptr{Void},(Ptr{Void},Ptr{Uint8},Ptr{Void},Bool,Ptr{Ptr{Uint8}},Ptr{Void},Ptr{Void}),dstdriver_ptr,destination,srcdataset,bstrict,options,C_NULL,C_NULL)
+end
+
+function gdal_create(destination::ASCIIString,dstdriver::ASCIIString,xsize::Int32,ysize::Int32,nbands::Int32,datatype::Int32,options::Array{ASCIIString,1})
+    dstdriver_ptr = gdal_get_driver_by_name(dstdriver)
+    ccall((:GDALCreate,"libgdal"),Ptr{Void},(Ptr{Void},Ptr{Uint8},Int32,Int32,Int32,Int32,Ptr{Ptr{Uint8}}),dstdriver_ptr,destination,xsize,ysize,nbands,datatype,options)
+end
+
+### High Level API functions
+
+function open_raster(input::ASCIIString,band::Int, access::Int32)
+    dataset = gdal_open(input,access)
+    raster = gdal_get_raster_band(dataset,int32(band))
+    (xsize,ysize) = gdal_get_raster_size(dataset)
+    raster_type = gdal_get_raster_datatype(raster)
+    raster_jtype = raster_type_convert(raster_type)
+    data = zeros(raster_jtype,xsize,ysize)
+    gdal_raster_io!(data, raster, raster_type,xsize,ysize,[int32(0), int32(0)])
+    transform = gdal_get_geotransform(dataset)
+    projection = gdal_get_projectionref(dataset)
+    map = Raster(dataset,xsize,ysize,transform,projection,data')
+    return map 
+end
+
+function copy_raster(raster::Raster,destination::ASCIIString,driver::ASCIIString)
+    dstdataset = gdal_create_copy(destination,raster.ptr,driver,false,ASCIIString[])
+end
+
+function write_raster(raster::Raster,destination::ASCIIString,driver::ASCIIString, datatype::Int32)
+    dstdataset = gdal_create(destination,driver,raster.width,raster.height,int32(1),datatype,ASCIIString[])
+    gdal_set_geotransform(dstdataset,raster.transform)
+    gdal_set_projection(dstdataset,raster.projection)
+    dstband = gdal_get_raster_band(dstdataset,int32(1))
+    gdal_raster_io(raster.data',dstband,datatype,raster.width,raster.height,[int32(0),int32(0)])
+    gdal_close(dstdataset)
+end
+
+function gdal_translate(source::ASCIIString,destination::ASCIIString,dstdriver::ASCIIString)
+    raster = open_raster(source,1,GA_ReadOnly)
+    copy_raster(raster,destination,dstdriver)
+end
 
 end
--- README.md
+++ README.md
@@ -9,6 +9,8 @@ Julia wrapper for the Geospatial Data Analysis library
 		ptr::Ptr{Void}
 		width::Int32
 		height::Int32
+		transform::Array{Float64,1}
+		projection::ASCIIString
 		data::Array{T,2}
 	end
 
@@ -16,15 +18,32 @@ A raster type consisting of a pointer to the raster dataset (not to the raster b
 
 # Provided Functions #
 
-	open_raster(input::String,band::Int32)
+	open_raster(input::String,band::Int32,access::Int32)
 
-Opens the specified band in the specified raster file using GDAL. returns a Raster.
+Opens the specified band in the specified raster file using GDAL. returns a Raster. Access is 0 for ReadOnly and 1 for Update, or use GA\_ReadOnly and GA\_Update
+
+	copy_raster(raster::Raster,destination::ASCIIString,driver::ASCIIString)
+
+Writes the specified raster object to the destination file using the given driver. Uses the GDAL CreateCopy() method.
+
+	write_raster(raster::Raster,destination::ASCIIString,driver::ASCIIString, datatype::Int32)
+
+Writes the specified raster object to the destination file using the given driver. Uses the GDAL Create() method. datatype is an integer specifying the GDALDataType of the written file. The enumerated types given by the API are exported (e.g. you can use GDT_Float32 to specify that datatype rather than using int32(6))
+
+	gdal_translate(source::ASCIIString,destination::ASCIIString,dstdriver::ASCIIString)
+
+Translates the source raster into the destination raster by opening the source as a Julia Raster then calling `copy_raster` on the Raster. Probably not as fast as just opening the dataset and copying the raster in C or through ccall since it has to read all of the raster data into the . More of a proof that the Raster type does what we want it to do.
+
+# Usage #
+
+If you had a raster map, say a digital elevation model, stored as a GeoTiff in "dem.tif," `dem = open_raster("dem.tif",1,GA_ReadOnly)` would return a Raster type object with fields giving you the width and height of the raster, the geotransform as output by GDAL, and the projection in OpenGIS WKT format. If you wanted to display the raster using PyPlot, for instance, `matshow(dem.data)` would display the raster. This doesn't yet support georeferenced plotting.
+
+Writing the loaded raster
 
 # Todo #
 
-1. Complete the GDAL datatype to Julia datatype conversion process. Currently, it can't handle complex data types.
-2. Currently the gdal_open and gdal_raster_io functions only take a subset of the arguments passed to the C functions for simplicity.
-
-
-		
-
+1. Figure out a better way to handle the GDALDatatype and Julia datatypes interface. Currently, it can't handle complex types, mostly because I haven't thought hard enough about the 
+2. Currently the gdal\_open and gdal\_raster_io functions only take a subset of the arguments passed to the C functions for simplicity.
+3. Write a parser for the projection strings, so we can do useful things with the projection.
+4. Come up with a good way to display rasters using the georeferenced coordinates.
+5. Add support for missing data. Maybe using DataArrays. How does GDAL handle missing data?
