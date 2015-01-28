a6e28e412ec41a932c1e484ceff9e163222a0604
Will Kearney <wkearn@gmail.com>
2015-01-28 11:13:56 -0500

    Remove unnecessary GDAL wrapper headers

--- src/gdal_csv.jl
+++
@@ -1,5 +0,0 @@
-# Julia wrapper for header: /opt/local/include/gdal_csv.h
-# Automatically generated using Clang.jl wrap_c, version 0.0.0
-
-@c Ptr{Uint8} GDALDefaultCSVFilename (Ptr{Uint8},) libgdal
-
--- src/gdal_version.jl
+++
@@ -1,4 +0,0 @@
-# Julia wrapper for header: /opt/local/include/gdal_version.h
-# Automatically generated using Clang.jl wrap_c, version 0.0.0
-
-
