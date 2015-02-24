89786a36c7ab9249db654be4bcd24bfcf2b0d93b
Will Kearney <wkearn@gmail.com>
2015-02-23 19:19:26 -0500

    Change tests to use a made-up multiband raster for reading and writing

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,35 +2,38 @@ using RasterIO
 using Base.Test
 
 # Check to make sure the drivers are properly registered
-# GeoTiff is built into GDAL, so this should work on all
+# GeoTiff and EHdr are built into GDAL, so this should work on all
 # systems
-@test check_create("GTiff",1) == true
-@test check_create("GTiff") == true
+@test check_create("GTiff",1)
+@test check_create("GTiff")
+@test check_create("EHdr",1)
+@test check_create("EHdr")
 
-# Open a sample raster file and check that its data matches
+# Open a tiny sample raster file and check that its data matches
 # expectations
-bushkill = open_raster("bushkill.tif")
-@test bushkill.width == 1052
-@test bushkill.height == 1390
-@test bushkill.transform == [489485.0,10.0,0.0,4.552435e6,0.0,-10.0]
-@test bushkill.projection == ""
+A = open_raster("A.tif")
+@test A.width == 3
+@test A.height == 3
+@test A.transform == zeros(6)
+@test A.projection == ""
 
-# Write opened raster to file. Again, RST is included with GDAL, so 
-# this should work as long as you have GDAL
-write_raster(bushkill,"bushkill.rst","RST",eval(parse(string("GDT_",eltype(bushkill.data)))))
+# Write opened raster to file.
+write_raster(A,"A","EHdr",eval(parse(string("GDT_",eltype(A.data)))))
 
 # Open the just-written raster and check that it matches the first
-bushkill2 = open_raster("bushkill.rst")
+A2 = open_raster("A")
 
-@test bushkill.data == bushkill2.data
-@test bushkill.width == bushkill2.width
-@test bushkill.height == bushkill2.height
-@test bushkill.transform == bushkill2.transform
+@test A.data == A2.data
+@test A.width == A2.width
+@test A.height == A2.height
+@test A.transform == A.transform
 # The projections won't be the same because of how RST stores projections
 
 #Remove the written raster files so as not to clutter things up
+
 for file in readdir()
-    if ismatch(r"bushkill.r",file)
+    if file == "A" || file == "A.hdr"
         rm(file)
     end
 end
+
