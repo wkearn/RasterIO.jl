292fdc73ecb1bd6f30705fa1309dea3da6c90d93
yeesian <ngyeesian@gmail.com>
2015-09-07 16:15:34 -0400

    clean up

--- src/RasterIO.jl
+++ src/RasterIO.jl
@@ -1,29 +1,11 @@
 module RasterIO
 
     const libgdal = "libgdal"
-        
-        # High-level API functions
-        # open_raster, copy_raster, write_raster,
-        # Utility functions
-        # driver_list, driver_test, check_create,
-        # Other useful functions
-        # gdal_translate,
-        # Constants
-        #GDT_Unknown, GDT_Byte, GDT_UInt16, GDT_Int16, GDT_UInt32, GDT_Int32, GDT_Float32, GDT_Float64,
-        #GA_ReadOnly, GA_Update,
-        #GF_Read, GF_Write
 
     include("gdal_api.jl")
     include("raster_types.jl")
     include("raster_functions.jl")
 
     GDALAllRegister() # register all known drivers
-
-    # ## Useful little functions
-
-    # function gdal_translate(source::ASCIIString,destination::ASCIIString,dstdriver::ASCIIString)
-    #     raster = open_raster(source,Int(GA_ReadOnly))
-    #     write_raster(raster,destination,dstdriver,eval(parse(string("GDT_",eltype(raster.data)))))
-    # end
 
 end
