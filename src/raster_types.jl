b5291b992b09c03bb913a225b35a6153f236b427
Marcio <marciolegal@hotmail.com>
2015-09-24 19:59:41 -0300

    show(raster) and print(raster) implementations

--- src/raster_types.jl
+++ src/raster_types.jl
@@ -1,3 +1,4 @@
+import Base.show, Base.print
 type Raster
     dataset::GDALDatasetH
     width::Cint
@@ -12,7 +13,24 @@ function Raster(dataset::GDALDatasetH)
                     _getrastercount(dataset)) # number of bands
     finalizer(raster, closeraster)
     raster
-end 
+end
+
+function Base.show(io::IO,raster::Raster)
+     if (raster.dataset == C_NULL)
+         println(io, raster.dataset)
+         return null;
+     end
+     # datatypes
+     if raster.nband==1
+         print("$(raster.height) x $(raster.width) (rows, cols) Single-Band Raster of type $(dtype(_getrasterband(raster.dataset, 1)))")
+     elseif raster.nband>1
+         print("$(raster.height) x $(raster.width) x $(raster.nband) (rows, cols, bands) Raster of types: \n")
+         for i=1:raster.nband
+             print("$(Char(1))   $(i): $(dtype(_getrasterband(raster.dataset, i))) \n")
+         end
+     end
+ end
+Base.print(io::IO, raster::Raster) =  show(io, raster::Raster)
 
 function closeraster(raster::Raster)
     if raster.dataset != C_NULL
