8367bf0eaed26a229c787a9530a72c6d6e5ad689
Fabian Gans <fabiangans@gmx.de>
2015-09-28 22:42:39 +0200

    Provide correct name for windows dll

--- deps/build.jl
+++ deps/build.jl
@@ -2,7 +2,7 @@ using BinDeps
 
 @BinDeps.setup
 
-libgdal = library_dependency("libgdal",aliases=["gdal"])
+libgdal = library_dependency("libgdal",aliases=["gdal","gdal200"])
 
 using Conda
 provides(Conda.Manager,"libgdal",libgdal)
